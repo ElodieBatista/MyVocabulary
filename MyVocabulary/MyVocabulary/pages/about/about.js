443cc3eb6cd3b89d75987f9acb163e42efe61694
elodiebatista <elodie.batista@live.fr>
2012-12-28 11:31:32 -0800

    Project Creation

---
+++ MyVocabulary/MyVocabulary/pages/about/about.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    "use strict";
+
+    var page = WinJS.UI.Pages.define("/pages/about/about.html", {
+        ready: function (element, options) {
+            // Apply translations
+            WinJS.Resources.processAll();
+        }
+    });
+})();
---
+++ MyVocabulary/MyVocabulary/pages/home/home.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    "use strict";
+
+    var page = WinJS.UI.Pages.define("/pages/home/home.html", {
+        ready: function (element, options) {
+            // Apply translations
+            WinJS.Resources.processAll();
+        }
+    });
+})();
